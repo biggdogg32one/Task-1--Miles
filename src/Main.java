44146fa1b153b3849fe922114b4ded04f79eb677
GIT Test Sergey <biggdogg97@gmail.com>
2022-11-17 17:04:51 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 100_000;
+        int percentRubles = 20;
+        int bonusMiles = 1;
+
+
+        int bonus = amount / percentRubles * 1;
+
+        System.out.println("Итоговые бонусные мили: " + bonus);
+    }
+}
